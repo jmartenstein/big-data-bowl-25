9aa9e5f269ef220e241ef9af690e8dbb7ddc96c1
Justin Martenstein <jmartenstein@knockcrm.com>
2024-11-06 15:17:19 -0800

    add passer validation script

--- src/validate_passers.py
+++ src/validate_passers.py
@@ -24,69 +24,58 @@ def validate_passer_by_description( player_row, detailed_play_row ):
     return_value = 0
     if match_text in play_detail:
         return_value = 1
+    else:
+        play_id = detailed_play_row["playId"].values[0]
+        print(f"{play_id}\tp: {player_name}\t{play_detail[:50]}")
 
     return return_value
 
-try:
-    game_id = int(sys.argv[1])
-except:
-    print("Invalid game id format")
-    sys.exit(1)
-
-tracking_file = "data/kaggle/tracking_week_8.csv"
+tracking_file = "data/kaggle/tracking_week_1.csv"
 df_tracking = pd.read_csv(tracking_file)
 
 df_plays = pd.read_csv("data/kaggle/plays.csv")
 df_players = pd.read_csv("data/kaggle/players.csv")
 
-df_game = df_tracking[
-            (df_tracking["gameId"] == game_id ) &\
-            (df_tracking["event"] == "pass_forward")
-]
-
-play_ids = df_game["playId"].unique()
-first_five_play_ids = play_ids[:5]
+total_passes = 0
 
 successful_validation = 0
 unsuccessful_validation = 0
 
-for i in play_ids:
+game_ids = df_tracking[ "gameId" ].unique()
 
-    df_play_frames = df_game[ df_game["playId"] == i ]
-    df_play_detailed = df_plays[
-        (df_plays["gameId"] == game_id) & \
-        (df_plays["playId"] == i)
+#game_ids = game_ids[:1]
+#game_ids = [ 2022091101, 2022091102, 2022091103 ]
+
+for g in game_ids:
+
+    df_game = df_tracking[
+                (df_tracking["gameId"] == g ) &\
+                (df_tracking["event"] == "pass_forward")
     ]
 
-    passer_id = ap.find_passer_id(df_play_frames)
-    passer_name = ap.get_player_name_by_id(df_players, passer_id)
+    play_ids = df_game["playId"].unique()
+    total_passes += len(play_ids)
+    #first_five_play_ids = play_ids[:5]
 
-    # get player row from dataframe
-    passer_row = df_players[ df_players["nflId"] == passer_id ]
+    for i in play_ids:
 
-    n,d = ap.get_closest_defender(df_play_frames, passer_name)
-    pass_outcome = df_play_detailed["passResult"].values[0]
-    pass_d = round(d, 2)
+        df_play_frames = df_game[ df_game["playId"] == i ]
+        df_play_detailed = df_plays[
+            (df_plays["gameId"] == g) & \
+            (df_plays["playId"] == i)
+        ]
 
-    rec_d = float('nan')
-    targeted_receiver_id = ap.find_targeted_receiver_id(df_play_frames, df_play_detailed)
-    if targeted_receiver_id != 0:
-        targeted_receiver_name = ap.get_player_name_by_id(df_players, targeted_receiver_id)
-        n,rec_d = ap.get_closest_defender(df_play_frames, targeted_receiver_name)
-        rec_d = round(rec_d, 2)
+        passer_id = ap.find_passer_id_by_closest_to_football(df_play_frames)
+        passer_name = ap.get_player_name_by_id(df_players, passer_id)
 
-    validate_result = validate_passer_by_description(passer_row, df_play_detailed)
+        # get player row from dataframe
+        passer_row = df_players[ df_players["nflId"] == passer_id ]
+        validate_result = validate_passer_by_description(passer_row, df_play_detailed)
 
-    successful_validation += validate_result
-    if validate_result == 0:
-        unsuccessful_validation += 1
-        print(f"{df_play_detailed['playId'].values[0]}: "
-              f"{df_play_detailed['playDescription'].values[0]}")
+        successful_validation += validate_result
+        if validate_result == 0:
+            unsuccessful_validation += 1
 
-    print(f"{i}: {passer_name}; pass sep: {pass_d}; rec sep: {rec_d}; "
-          f"res: {pass_outcome}; {validate_result}")
-
-#print("Hello World")
 print()
-print(f"Total passes found: {len(play_ids)}; "
-      f"valid passers = {successful_validation}; invalid passers = {unsuccessful_validation}")
+print(f"Total passes found: {total_passes}; "
+      f"matching passers = {successful_validation}; non-matching passers = {unsuccessful_validation}")
