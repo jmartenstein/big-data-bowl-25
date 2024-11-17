b7eea78a274cc9eebf004a372047f6475681f5cf
Justin Martenstein <jmartenstein@knockcrm.com>
2024-11-16 16:02:45 -0800

    generate pass result set only for completed passes

--- src/generate_pass_result_set.py
+++ src/generate_pass_result_set.py
@@ -13,18 +13,28 @@ df_gs = pd.read_csv("data/kaggle/games.csv")
 df_gs_week1 = df_gs[ df_gs["week"] == 1 ]
 
 list_games_week1 = df_gs_week1[ "gameId" ].unique()
-#print(list_games_week1)
 
-# load player play data
+# load plays data,limit to week1
+df_ps = pd.read_csv("data/kaggle/plays.csv")
+df_ps_week1 = df_ps[ df_ps["gameId"].isin(list_games_week1) ]
+
+# filter only the gameId and playId (indices), and the pass result
+df_pr_week1 = df_ps_week1[[ "gameId", "playId", "passResult" ]]
+
+# load player play data, limit to week 1
 df_pp = pd.read_csv("data/kaggle/player_play.csv")
+df_pp_week1 = df_pp[ df_pp["gameId"].isin(list_games_week1) ]
 
-# limit player play data to week 1
-df_pp_week1 = df_pp[ df_pp["gameId"].isin(list_games_week1) ].copy()
+# merge pass result into every line of player play data
+df_merge = df_pp_week1.merge(df_pr_week1, on=['gameId','playId'])
+
+# reduce player play data to only plays with complete passes
+df_cp_week1 = df_merge[ df_merge["passResult"] == "C" ].copy()
 
 # create is_passer and is_receiver columns by applying function
-df_pp_week1["isPasser"] = df_pp_week1.apply(is_passer, axis=1)
-df_pp_week1["isReceiver"] = df_pp_week1.apply(is_receiver, axis=1)
+df_cp_week1["isPasser"] = df_cp_week1.apply(is_passer, axis=1)
+df_cp_week1["isReceiver"] = df_cp_week1.apply(is_receiver, axis=1)
 
 # write result set to csv
 header = [ "gameId", "playId", "nflId", "isPasser", "isReceiver" ]
-df_pp_week1.to_csv('data/pass_result_set.csv', columns=header, index=False)
+df_cp_week1.to_csv('data/pass_result_set.csv', columns=header, index=False)
