44d3b623a6cdcf1d94df1044f4d889fd0698fb69
Justin Martenstein <jmartenstein@knockcrm.com>
2024-11-19 17:16:28 -0800

    fix script to validate passers

--- src/validate_passers.py
+++ src/validate_passers.py
@@ -56,39 +56,22 @@ successful_validation = 0
 unsuccessful_validation = 0
 
 game_ids = df_tracking[ "gameId" ].unique()
-
-#game_ids = game_ids[:1]
 #game_ids = [ 2022091101, 2022091102, 2022091103 ]
 
 df_passing_plays = get_confirmed_passing_plays( df_plays )
 df_passing_plays_week_1 = df_passing_plays[ df_passing_plays["gameId"].isin(game_ids) ]
-
 print(df_passing_plays_week_1.shape)
 
-first_five_plays = df_passing_plays_week_1[:5]
-
-#first_five_plays.apply()
-
-sys.exit(0)
-
 for g in game_ids:
-
-    #df_game = df_tracking[
-    #            (df_tracking["gameId"] == g ) &\
-    #            (df_tracking["event"] == "pass_forward")
-    #]
 
     df_pass_plays_per_game = df_passing_plays_week_1[ df_passing_plays_week_1["gameId"] == g ]
     play_ids = df_pass_plays_per_game["playId"].unique()
 
     total_passes += len(play_ids)
-    #first_five_play_ids = play_ids[:5]
-
-    #print(play_ids)
 
     for p in df_pass_plays_per_game:
 
-        i = df_pass_plays_per_game["playId"].values(0)
+        i = df_pass_plays_per_game["playId"].values[0]
 
         df_play_frames = df_tracking[ df_tracking["playId"] == i ]
         df_play_detailed = df_plays[
@@ -98,7 +81,11 @@ for g in game_ids:
 
         #print(df_play_detailed.shape)
 
-        passer_id = ap.find_passer_id_by_closest_to_football(df_play_frames)
+        frame_id = ap.get_frame_id_for_event(df_play_frames, "pass_forward")
+        if (frame_id < 0):
+            frame_id = ap.get_frame_id_for_event(df_play_frames, "pass_shovel")
+
+        passer_id = ap.find_player_id_by_closest_to_football(df_play_frames, frame_id)
         passer_name = ap.get_player_name_by_id(df_players, passer_id)
 
         # get player row from dataframe
