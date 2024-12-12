236e02a9f82452bb65ba74ed837ec820c675a9be
Justin Martenstein <jmartenstein@knockcrm.com>
2024-12-12 15:43:45 -0800

    add incomplete passes

--- src/explore_pass_motion_yardage.py
+++ src/explore_pass_motion_yardage.py
@@ -11,14 +11,20 @@ list_games_week1 = df_gs_week1[ "gameId" ].unique()
 df_pp = pd.read_csv("data/kaggle/player_play.csv")
 df_pp_week1 = df_pp[ df_pp["gameId"].isin(list_games_week1) ]
 
-# get plays with motion and top 10 yards after catch
-df_yardage_gained = df_pp_week1[ df_pp_week1[ "yardageGainedAfterTheCatch" ] > 0 & \
+# get plays with motion and yards after catch
+df_yardage_gained = df_pp_week1[ df_pp_week1[ "yardageGainedAfterTheCatch" ] > 10 & \
                                  df_pp_week1[ "motionSinceLineset" ] ].copy()
-
-print(df_yardage_gained.shape)
-
 df_yg_sorted = df_yardage_gained.sort_values(by="yardageGainedAfterTheCatch",
                                              ascending=False)
-
-df_top = df_yg_sorted[:20]
+df_top = df_yg_sorted[:10]
 print(df_top[[ "gameId", "playId", "nflId", "yardageGainedAfterTheCatch" ]])
+print()
+
+# get plays with motion, between 10 and 20 yards target past the line
+# of scrimmage and incomplete
+df_incomplete = df_pp_week1[ ( df_pp_week1[ "wasTargettedReceiver" ] > 0 ) & \
+                             ( df_pp_week1[ "motionSinceLineset" ] == True ) & \
+                             ( df_pp_week1[ "hadPassReception" ] == 0 ) ].copy()
+#df_incomplete_sorted = df_incomplete,sort_values(by="passLength", ascending=False)
+df_incomplete_top = df_incomplete[:10]
+print(df_incomplete_top[[ "gameId", "playId", "nflId", "wasTargettedReceiver"]])
