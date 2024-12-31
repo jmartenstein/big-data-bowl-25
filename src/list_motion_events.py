b4696a24b0d4813a2719e86354d13dc94c0e37b3
Justin Martenstein <jmartenstein@knockcrm.com>
2024-12-31 12:38:24 -0800

    loop multiple plays and games

--- src/list_motion_events.py
+++ src/list_motion_events.py
@@ -4,6 +4,7 @@ import analyze_play as ap
 import sys
 import math
 
+from datetime import datetime
 from itertools import groupby
 from operator import itemgetter
 
@@ -16,7 +17,22 @@ ap.DATA_DIR = "data/kaggle"
 SPEED_THRESHOLD = 0.9  # yards / sec
 TIME_THRESHOLD  = 0.5  # seconds (multiply * 10 for frame count)
 
+
 ### FUNCTIONS ###
+
+def get_motion_event_filename(game_id=''):
+
+    now = datetime.now()
+
+    s_date = now.strftime("%Y%m%d")
+    s_time = now.strftime("%H%M%S")
+
+    if game_id:
+        s_prefix = f"motion.{game_id}"
+    else:
+        s_prefix = "motion"
+
+    return f"{s_prefix}.{s_date}.{s_time}.csv"
 
 def get_opposite_dir(direction):
 
@@ -36,6 +52,15 @@ def get_opposite_dir(direction):
         opposite = "forward"
 
     return opposite
+
+def get_plays_from_game( game_id ):
+
+    plays_file = ap.DATA_DIR + "/plays.csv"
+    df_all_plays = pd.read_csv(plays_file)
+
+    df_game_plays = df_all_plays[ df_all_plays[ "gameId" ] == game_id ]
+
+    return df_game_plays["playId"].unique()
 
 def get_min_frame_from_events( df, l_events ):
     min_frame = 500
@@ -113,7 +138,6 @@ def get_motion_event_frames_by_player(df, p_id):
     if (len(df_)) > 0:
 
         frames = df_[ "frameId" ].unique()
-        #print(frames)
 
         # group frames by consecutive numbers
         for k, g in groupby(enumerate(frames), lambda i_x: i_x[0] - i_x[1]):
@@ -121,7 +145,6 @@ def get_motion_event_frames_by_player(df, p_id):
             if len(l_group) > (TIME_THRESHOLD * 10):
                 l_grouped_frames.append(l_group)
 
-    # return list of frame id's
     return l_grouped_frames
 
 def summarize_motion_event(df, l_frames, player_id, is_def):
@@ -144,7 +167,7 @@ def summarize_motion_event(df, l_frames, player_id, is_def):
     motion_dir, dir_offset = get_general_direction_and_offset(vector_x, vector_y, s_play_dir)
 
     if abs(dir_offset) > 45:
-        print(f"WARN: {name:17} ({p}): {f_start:3} - {f_end:3}; {motion_dir:6} {dir_offset}")
+        print(f"WARN: {name:17} ({player_id}): {f_start:3} - {f_end:3}; {motion_dir:6} {dir_offset}")
 
     total_distance = math.sqrt((vector_x ** 2) + (vector_y ** 2))
     abs_speed = total_distance / motion_time
@@ -157,30 +180,26 @@ def summarize_motion_event(df, l_frames, player_id, is_def):
     return [ f_start, f_end, vector_x, vector_y, motion_dir, dir_offset,
              team_dir, total_distance, abs_speed ]
 
+def get_motion_events( df_f, df_d ):
 
-def get_motion_events( game_id, play_id ):
-
-    df_f, df_d = ap.load_tracking_from_game_and_play(game_id, play_id)
-    df_pp = pd.read_csv(f"{ap.DATA_DIR}/player_play.csv")
+    game_id = df_d["gameId"].values[0]
+    play_id = df_d["playId"].values[0]
 
     defense_team = df_d["defensiveTeam"].values[0]
-    print(df_d["playDescription"].values[0])
-    #print(df_f.shape)
 
     start_events = [ "line_set", "man_in_motion" ]
     end_events = [ "ball_snap" ]
 
     presnap_start = get_min_frame_from_events(df_f, start_events)
     presnap_end = get_max_frame_from_events(df_f, end_events)
-    print(f"{presnap_start}, {presnap_end}")
 
     df_pre = get_presnap_dataframe(df_f, presnap_start, presnap_end)
-    #print(df_pre)
-
     p_ids = get_player_ids(df_pre)
 
     l_motions = []
     motion_idx = 11
+
+    df_pp = pd.read_csv(f"{ap.DATA_DIR}/player_play.csv")
 
     for p in p_ids:
 
@@ -211,24 +230,70 @@ def get_motion_events( game_id, play_id ):
     return pd.DataFrame( l_motions, columns=columns )
 
 
-
 ### MAIN ###
 
-if (len(sys.argv) < 3):
+if (len(sys.argv) < 2):
     print("Specify gameId and playId")
     sys.exit(1)
 
 try:
-    g_id = int(sys.argv[1])
+    g_id = sys.argv[1]
 except:
     print("Invalid game id format")
     sys.exit(1)
 
+p_id = ""
 try:
     p_id = int(sys.argv[2])
 except:
-    print("Invalid play id format")
-    sys.exit(1)
+    print(f"WARN: No play specified, pulling all plays for game(s): {g_id}")
 
-df_out = get_motion_events( g_id, p_id )
-print(df_out)
+game_list = g_id.split(",")
+
+df_out = pd.DataFrame([])
+
+for g in game_list:
+
+    tracking_file = ap.get_tracking_file_for_week( int(g) )
+    plays_file = ap.DATA_DIR + "/plays.csv"
+
+    df_plays = pd.read_csv(plays_file)
+    df_tracking = pd.read_csv(tracking_file)
+
+    df_game_frames = ap.filter_frames_by_game( df_tracking, int(g) )
+    df_game_plays = ap.filter_frames_by_game( df_plays, int(g) )
+
+    if p_id:
+        list_plays = [ p_id ]
+    else:
+        list_plays = get_plays_from_game( int(g) )
+
+    count = 0
+
+    print(f"Progress ({g}): ", end='', flush=True)
+
+    for p in list_plays:
+
+        # the for loop is slow, print a progress bar; maybe we can use a group_by
+        # here instead?
+        count += 1
+        if (count % 10) == 0:
+            print('.', end='', flush=True)
+
+        df_play_tracking_frames = ap.filter_frames_by_play( df_game_frames, p )
+        df_play_details = ap.filter_frames_by_play( df_game_plays, p)
+
+        df_motion_events = get_motion_events( df_play_tracking_frames, df_play_details )
+
+        df_out = pd.concat( [ df_out, df_motion_events ] )
+
+    print()
+
+s_game_id = ""
+if len(game_list) == 1:
+    s_game_id = game_list[0]
+
+outfile = get_motion_event_filename(s_game_id)
+
+print(f"DataFrame (shape: {df_out.shape}) to file {outfile}")
+df_out.to_csv(f"data/processed/{outfile}", index=False)
